b3de32b7d17b7cc506bd001b53a69b8143f46ab7
eric12345678910 <ericlaudrum@gmail.com>
2025-01-24 16:45:03 -0500

    Add weapon roll to player's combat strength

--- AWESOME-CODEBASES/lab02.py
+++ AWESOME-CODEBASES/lab02.py
@@ -11,43 +11,45 @@ mCombatStrength = int(input("Enter the monster's combat Strength: "))
 weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
 
 # Health Points
-input("Roll the dice for your health points (Press enter)")
+input("\nRoll the dice for your health points (Press enter)")
 healthPoints = random.choice(diceOptions)
 print("You rolled " + str(healthPoints) + " health points")
 
-input("Roll the dice for the monster's health points (Press enter)")
+input("\nRoll the dice for the monster's health points (Press enter)")
 mHealthPoints = random.choice(diceOptions)
 print("You rolled " + str(mHealthPoints) + " health points for the monster")
 
 # Healing Potion
-input("Roll the dice to see if you find a healing potion (Press enter)")
+input("\nRoll the dice to see if you find a healing potion (Press enter)")
 healingPotion = random.choice([0, 1])
 print("Have you found a healing potion?: " + str(bool(healingPotion)))
 
 # Weapon Select
-input("Roll the dice to select your weapon (Press enter)")
+input("\nRoll the dice to select your weapon (Press enter)")
 numWeapons = len(weapons)
 weaponRoll = random.choice(diceOptions)
-print("\nYou rolled a " + str(weaponRoll))
-print("You pull out a mighty " + weapons[(int(weaponRoll)-1)] + "\n")
+combatStrength += int(weaponRoll)
+print("You rolled a " + str(weaponRoll))
+print("Your combat strength increases to " + str(combatStrength) + " as you...")
+print("reveal your mighty " + weapons[(int(weaponRoll)-1)])
 
 
 
 
 
 # Analyze the Roll
-input("Analyze the roll (Press enter)")
+input("\nAnalyze the roll (Press enter)")
 # Equality operators
-print("\n--- You are matched in strength: " + str(combatStrength == mCombatStrength))
+print("--- You are matched in strength: " + str(combatStrength == mCombatStrength))
 
 # Relational operators
-print("\n--- You have a strong player: " + str((combatStrength + healthPoints) >= 15))
+print("--- You have a strong player: " + str((combatStrength + healthPoints) >= 15))
 
 # and keyword
-print("\n--- Remember to take a healing potion!: " + str(healingPotion == 1 and healthPoints <= 6))
+print("--- Remember to take a healing potion!: " + str(healingPotion == 1 and healthPoints <= 6))
 
 # not keyword
-print("\n--- Phew, you have a healing potion: " + str(
+print("--- Phew, you have a healing potion: " + str(
     not (                               # monster will NOT kill hero in one blow
         healthPoints < mCombatStrength  # monster will kill hero in one blow
     )
@@ -56,20 +58,20 @@ print("\n--- Phew, you have a healing potion: " + str(
 ))
 
 # or keyword
-print("\n--- Things are getting dangerous: " + str(healingPotion == 0 or healthPoints == 1))
+print("--- Things are getting dangerous: " + str(healingPotion == 0 or healthPoints == 1))
 
 # in keyword
-print("\n--- Is it possible to roll 0 in the dice?: " + str(0 in diceOptions))
+print("--- Is it possible to roll 0 in the dice?: " + str(0 in diceOptions))
 
 # --- Expanded if statement
 if healthPoints >= 5:
-    print("\n--- Your health is ok")
+    print("--- Your health is ok")
 elif healingPotion == 1:
     healingPotion = 0
     healthPoints = 6
-    print("\n--- Using your healing potion... Your Health Points is now full at " + str(healthPoints))
+    print("--- Using your healing potion... Your Health Points is now full at " + str(healthPoints))
 else:
-    print("\n--- Your health is low at " + str(healthPoints) + " and you have no healing potions available!")
+    print("--- Your health is low at " + str(healthPoints) + " and you have no healing potions available!")
 
 
 # --- Nested if statement
